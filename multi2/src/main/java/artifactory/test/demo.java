1567f95af6bca4d0414025e77d988320768dde76
mrajsekhar <98088968+mrajsekhar@users.noreply.github.com>
2022-03-05 21:52:27 +0530

    Create demo.java

---
+++ multi2/src/main/java/artifactory/test/demo.java
@@ -0,0 +1,13 @@
+package artifactory.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class demo 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
